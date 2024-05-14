3ff3f416511212b13e0659e0ecb8bbeee0eb85f4
bi-andenghuo <62098894+bi-andenghuo@users.noreply.github.com>
2024-05-14 21:33:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ let addresses = [
 
 let sub = '';// 'trojan.fxxk.dedyn.io' 
 let subconverter = 'apiurl.v1.mk';// clash订阅转换后端，目前使用肥羊的订阅转换功能。自带虚假节点信息防泄露
-let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini.ini"; //订阅配置文件
+let subconfig = "https://github.com/ACL4SSR/ACL4SSR/blob/master/Clash/config/ACL4SSR_NoApple.ini"; //订阅配置文件
 let RproxyIP = 'false';
 
 let addressesapi = [];
